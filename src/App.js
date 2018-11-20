4e8a88b2f13ddaa2e2cb0fad886ae418fb888592
estebanfloresf <esteban_fs@hotmail.com>
2018-11-20 11:01:38 -0500

    animation with emotion

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Link } from '@reach/router';
+import { Router } from '@reach/router';
 import Results from './Results';
 import Details from './Details';
+import NavBar from './NavBar';
 import SearchParams from './SearchParams';
 import pf from 'petfinder-client';
 import { Provider } from './SearchContext';
@@ -73,14 +74,7 @@ class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<header>
-					<Link to="/"> Adopt Me </Link>
-					<Link to="/search-params">
-						<span aria-label="search" role="img">
-							🔍
-						</span>
-					</Link>
-				</header>
+				<NavBar />
 				<Provider value={this.state}>
 					<Router>
 						<Results path="/" />
